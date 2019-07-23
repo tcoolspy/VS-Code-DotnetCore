bb51c382b30e3d85e67d7d02ebddf783d1561578
Travis L. Spencer <travis.spencer@regions.com>
2019-07-23 11:54:13 -0500

    adding solution for Part 2

---
+++ training/IntroToDotNetCore/Part2/dotnetPartTwo.Core/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dotnetPartTwo.Core.Models
+{
+    public class Student
+    {
+        public Guid Id {get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Email {get;set;}        
+    }
+}
