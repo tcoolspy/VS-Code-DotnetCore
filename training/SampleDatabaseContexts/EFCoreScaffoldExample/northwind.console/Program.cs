885a542aea0eba3c04088f5da33d2365e074a6dd
Travis L. Spencer <travis.spencer@regions.com>
2019-08-16 13:34:22 -0500

    adding sample dbcontexts

---
+++ training/SampleDatabaseContexts/EFCoreScaffoldExample/northwind.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace northwind.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
